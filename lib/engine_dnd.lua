6063c19d283e0c00614759b13bb6f45273bf6593
Josue Arias <31876967+synthi@users.noreply.github.com>
2025-01-04 00:39:40 -0300

    Update engine_dnd.lua
    amp values  /10 extended filter frequency

--- lib/engine_dnd.lua
+++ lib/engine_dnd.lua
@@ -1,6 +1,6 @@
 -- engine_dnd
 -- by: @cfd90
---
+-- amp mod by synthi
 
 local dnd = {}
 
@@ -8,7 +8,7 @@ dnd.add_params = function()
   -- Setup params.
   params:add_separator("osc 1")
   
-  params:add_control("osc1Amp", "osc 1 amp", controlspec.new(0.0, 1, 'lin', 0.01, 0, '', 1/500))
+  params:add_control("osc1Amp", "osc 1 amp", controlspec.new(0.0, 0.1, 'lin', 0.01, 0, '', 1/500))
   params:set_action("osc1Amp", function(x) engine.osc1Amp(x) end)
   
   params:add_control("osc1Freq", "osc 1 freq", controlspec.new(1, 2000, 'exp', 0.01, 55, 'hz', 1/500))
@@ -26,7 +26,7 @@ dnd.add_params = function()
   
   params:add_separator("osc 2")
   
-  params:add_control("osc2Amp", "osc 2 amp", controlspec.new(0.0, 1, 'lin', 0.01, 0, '', 1/500))
+  params:add_control("osc2Amp", "osc 2 amp", controlspec.new(0.0, 0.1, 'lin', 0.01, 0, '', 1/500))
   params:set_action("osc2Amp", function(x) engine.osc2Amp(x) end)
   
   params:add_control("osc2Freq", "osc 2 freq", controlspec.new(1, 2000, 'exp', 0.01, 110, 'hz', 1/500))
@@ -44,7 +44,7 @@ dnd.add_params = function()
   
   params:add_separator("osc 3")
   
-  params:add_control("osc3Amp", "osc 3 amp", controlspec.new(0.0, 1, 'lin', 0.01, 0, '', 1/500))
+  params:add_control("osc3Amp", "osc 3 amp", controlspec.new(0.0, 0.1, 'lin', 0.01, 0, '', 1/500))
   params:set_action("osc3Amp", function(x) engine.osc3Amp(x) end)
   
   params:add_control("osc3Freq", "osc 3 freq", controlspec.new(1, 2000, 'exp', 0.01, 220, 'hz', 1/500))
@@ -70,13 +70,13 @@ dnd.add_params = function()
   
   params:add_separator("noise")
 
-  params:add_control("noiseAmp", "noise amp", controlspec.new(0.0, 1, 'lin', 0.01, 0.00, '', 1/500))
+  params:add_control("noiseAmp", "noise amp", controlspec.new(0.0, 0.1, 'lin', 0.01, 0.00, '', 1/500))
   params:set_action("noiseAmp", function(x) engine.noiseAmp(x) end)
   
-  params:add_control("filterFreq", "filter freq", controlspec.new(1, 10000, 'exp', 0.01, 200, 'hz', 1/500))
+  params:add_control("filterFreq", "filter freq", controlspec.new(1, 12000, 'exp', 0.01, 200, 'hz', 1/500))
   params:set_action("filterFreq", function(x) engine.filterFreq(x) end)
   
-  params:add_control("filterRes", "filter res", controlspec.new(0.1, 3.95, 'exp', 0.01, 0.2, '', 1/500))
+  params:add_control("filterRes", "filter res", controlspec.new(0.1, 3.98, 'exp', 0.01, 0.2, '', 1/500))
   params:set_action("filterRes", function(x) engine.filterRes(x) end)
 end
 
